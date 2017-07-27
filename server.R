fa8c68a59b7f4f7c4963bf8b79787a2ef98d6ffb
mbsabath <bsabath1@gmail.com>
2017-07-27 13:59:54 -0400

    Added comments, implemented file uploading, example data

--- server.R
+++ server.R
@@ -17,30 +17,63 @@ source("utils.R")
 
 shinyServer(function(input, output) {
   
-  output$ui <- renderUI({
-    lapply(1:input$cfacts, function(j) {
-      column(3,
-             lapply(1:length(peacef), function(i){
-               numericInput(paste0(LETTERS[j],"var",i), names(peacef)[i], 
-                            median(peacef[,i], na.rm = T))
-             }))
-    })
+  ## Generate Custom Counterfactual Fields
+  output$slider <- renderUI({
+    if (input$cfactType) {} else{
+      sliderInput("cfacts", "Number of Counterfactuals",
+                  min = 1, max = 4, value = 1)
+    }
   })
   
+  output$ui <- renderUI({
+    if (input$cfactType) {
+      sidebarPanel(fileInput("counterfact",'Upload a Counterfactual Dataframe (.RDS)',
+                accept = '.RDS'),"Note: The counterfactual dataframe must be
+                numeric and have the same number of columns as the primary data. File should 
+                be saved using the saveRDS() function.")
+      
+    } else {
+    lapply(1:input$cfacts, function(j) {
+      column(3,
+             lapply(1:length(ShinyData()), function(i){
+               numericInput(paste0(LETTERS[j],"var",i), names(ShinyData())[i], 
+                            median(ShinyData()[,i], na.rm = T))
+             }))
+    })
+    }
+  })
+  
+  ## handle data upload, use peacef as default
+  ShinyData <- reactive({
+    dataFile <- input$data
+    if (is.null(input$data)) {
+      peacef
+    } else {
+      readRDS(dataFile$datapath)
+    }
+  })
+  ## Combine Counterfactuals into a matrix or handle uploaded data
   counterfacts <- reactive({
+    if (input$cfactType){
+      myFile <- input$counterfact
+      if(is.null(myFile)) {peacecf[1:4,]} else {
+      readRDS(myFile$datapath)
+      }
+    } else {
     cfacts <- input$cfacts
     out <- vector()
-    for (j in 1:length(peacef)) {
+    for (j in 1:length(ShinyData())) {
       for (i in 1:cfacts) {
         out <- c(out, input[[paste0(LETTERS[i],"var",j)]])
       }
     }
-   matrix(out, nrow = cfacts)
+   matrix(out, nrow = cfacts)}
     ##names(out) <- names(peacef)
   })
   
-  x <- reactive({
-    whatif(data = peacef, cfact = counterfacts())
+  ## Run whatif
+  x <- eventReactive(input$update,{
+    whatif(data = ShinyData(), cfact = counterfacts())
   })
   
   output$cfact <- renderTable({counterfacts()})
--- ui.R
+++ ui.R
@@ -14,20 +14,35 @@ shinyUI(fluidPage(
   titlePanel("Whatif Visualization Prototype"),
   # Show a plot of the generated distribution
   tabsetPanel(
-    tabPanel("Counterfactual", fluidRow(
-      tableOutput("cfact")
-    )),
     tabPanel("Heatmap", fluidRow(
       plotOutput("distPlot")
     )),
     tabPanel("Line Graph", fluidRow(
       plotOutput("linePlot")
+    )),
+    tabPanel("Upload Data",fluidRow(
+      sidebarPanel(
+        fileInput("data",label = "Upload Data", accept = ".RDS"),
+        "Upload an R data frame to use as the factual data with whatif. The data
+        should be saved using the saveRDS() function. If no data is uploaded, this
+        visualization will be based off the peacef data included in the whatif package."
+      )
+    )
+             ),
+    tabPanel("Counterfactual", fluidRow(
+      tableOutput("cfact")
     ))
   ),
+  hr(),
   fluidRow(
-    sliderInput("cfacts", "Number of Counterfactuals",
-                min = 1, max = 4, value = 1)
+    sidebarPanel(
+    radioButtons("cfactType",label = "Counterfactual Input",
+                 choices = list("Upload Dataframe" = T, "Manual Entry" = F),
+                 selected = T),
+    actionButton("update", "Update Graph"))
   ),
+    uiOutput("slider")
+  ,
   fluidRow(
     uiOutput("ui")
   )
